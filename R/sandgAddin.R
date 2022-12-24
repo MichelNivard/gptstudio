3e13789c6935ae6bfb82da92eac18877f0e2b14c
Michel Nivard <m.g.nivard@gmail.com>
2022-12-24 21:45:50 +0100

    Add files via upload
    First commit, hurray

---
+++ R/sandgAddin.R
@@ -0,0 +1,23 @@
+#' Insert \%in\%.
+#'
+#' Call this function as an addin to ask GPT tp improve spelling and grammar of selected text
+#'
+#' @export
+sandgAddin <- function() {
+   selection <- rstudioapi::getActiveDocumentContext()
+
+   print(selection$contents)
+   edit <- create_edit(
+     model = "text-davinci-edit-001",
+     input = selection$contents,
+     instruction = "Improve spellign and grammar",
+     temperature = 1,
+     top_p = 1,
+     openai_api_key = Sys.getenv("OPENAI_API_KEY"),
+     openai_organization = NULL
+   )
+
+   rstudioapi::insertText("edit")
+
+
+}
