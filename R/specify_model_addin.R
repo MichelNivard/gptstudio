fd3f2245b7a3d1e05a41039b2784c95c55605aff
James Wade <github@jameshwade.com>
2022-12-30 09:31:14 -0500

    tweak app layout for model specification

--- R/specify_model_addin.R
+++ R/specify_model_addin.R
@@ -6,5 +6,5 @@
 specify_model_addin <- function() {
   check_api()
   withr::local_options(shiny.launch.browser = .rs.invokeShinyPaneViewer)
-  shiny::runApp(system.file(package = "gpt_tools", "gpt_specify_model"))
+  shiny::runApp(system.file(package = "gptstudio", "gpt_specify_model"))
 }
