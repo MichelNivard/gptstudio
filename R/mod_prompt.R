46e87a8bee6a5ae726b44e68aea6445dcd513800
calderonsamuel <samuel.calderon@uarm.pe>
2023-04-16 01:20:13 -0500

    show a default welcome message at start

--- R/mod_prompt.R
+++ R/mod_prompt.R
@@ -52,10 +52,11 @@ mod_prompt_ui <- function(id) {
   )
 }
 
-mod_prompt_server <- function(id, rv) {
+mod_prompt_server <- function(id) {
     moduleServer(id, function(input, output, session) {
 
-      rv$all_chats_formatted <- NULL
+      rv <- reactiveValues()
+      rv$all_chats_formatted <- make_chat_history(chat_message_default())
       rv$all_chats <- NULL
 
       shiny::observe({
@@ -80,9 +81,14 @@ mod_prompt_server <- function(id, rv) {
       }) %>%
         shiny::bindEvent(input$chat)
 
-      shiny::observe(rv$all_chats <- NULL) %>%
+      shiny::observe({
+        rv$all_chats <- NULL
+        rv$all_chats_formatted <- make_chat_history(chat_message_default())
+      }) %>%
         shiny::bindEvent(input$clear_history)
 
+      # module return ----
+      rv
     })
 }
 
@@ -180,6 +186,64 @@ chat_message <- function(message) {
       class = glue("p-2 mb-2 rounded d-inline-block w-auto mw-100 {bg_class}"),
       fontawesome::fa(icon_name),
       shiny::markdown(message$content)
+    )
+  )
+}
+
+chat_message_default <- function() {
+
+  welcome_messages <- c(
+    "Welcome to the R programming language! I'm here to assist you in your journey, no matter your skill level.",
+    "Hello there! Whether you're a beginner or a seasoned R user, I'm here to help.",
+    "Hi! I'm your virtual assistant for R. Don't be afraid to ask me anything, I'm here to make your R experience smoother.",
+    "Greetings! As an R virtual assistant, I'm here to help you achieve your coding goals, big or small.",
+    "Welcome aboard! As your virtual assistant for R, I'm here to make your coding journey easier and more enjoyable.",
+    "Nice to meet you! I'm your personal R virtual assistant, ready to answer your questions and provide support.",
+    "Hi there! Whether you're new to R or an experienced user, I'm here to assist you in any way I can.",
+    "Hello! As your virtual assistant for R, I'm here to help you overcome any coding challenges you might face.",
+    "Welcome to the world of R! I'm your virtual assistant, here to guide you through the process of mastering this powerful language.",
+    "Hey! I'm your personal R virtual assistant, dedicated to helping you become the best R programmer you can be.",
+    "Greetings and welcome! I'm here to assist you on your R journey, no matter where you're starting from.",
+    "Hi, I'm your R virtual assistant! My goal is to help you achieve success in your coding endeavors, whatever they may be.",
+    "Hello and welcome! As your virtual assistant for R, I'm here to make your coding experience more efficient and productive.",
+    "Hey there! I'm your personal R virtual assistant, ready to help you take your coding skills to the next level.",
+    "Greetings! Whether you're a beginner or an experienced R user, I'm here to provide support and assistance.",
+    "Hello and welcome to R! I'm your virtual assistant, and I'm excited to help you on your coding journey.",
+    "Hey! I'm here to help you with all things R, no matter what your skill level is.",
+    "Greetings and salutations! As your R virtual assistant, I'm here to provide the guidance and support you need to succeed.",
+    "Welcome to the wonderful world of R! I'm your personal virtual assistant, ready to assist you in your coding journey.",
+    "Hi there! Whether you're just starting out or a seasoned R user, I'm here to help you reach your coding goals.",
+    "Hello and welcome to R! I'm your virtual assistant, and I'm here to help you navigate this powerful language with ease.",
+    "Hey! I'm your personal R virtual assistant, and I'm dedicated to helping you achieve success in your coding endeavors.",
+    "Greetings! As your virtual assistant for R, I'm here to help you become a confident and proficient R user.",
+    "Welcome to the R community! I'm your virtual assistant, and I'm here to support you every step of the way.",
+    "Hi there! I'm your personal R virtual assistant, and I'm committed to helping you achieve your coding goals."
+  )
+
+  paperplane <- fontawesome::fa("fas fa-paper-plane") |> as.character()
+  eraser <- fontawesome::fa("eraser")
+  gear <- fontawesome::fa("gear")
+
+  explain_btns <- glue(
+    "In this chat you can:
+
+    - Send me a prompt ({paperplane})
+    - Clear the current chat history ({eraser})
+    - Change the settings ({gear})"
+  )
+
+  content <- glue(
+    "{sample(welcome_messages, 1)}
+
+    {explain_btns}
+
+    Type anything to start our conversation."
+  )
+
+  list(
+    list(
+      role = "assistant",
+      content = content
     )
   )
 }
