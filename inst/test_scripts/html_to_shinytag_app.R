dc89d9b9c75bf952b42ec8fb3988f6807b970b8f
calderonsamuel <samuel.calderon@uarm.pe>
2023-04-20 16:55:34 -0500

    html_to_r works as expected

--- inst/test_scripts/html_to_shinytag_app.R
+++ inst/test_scripts/html_to_shinytag_app.R
@@ -48,54 +48,6 @@ test_html_2 <-
   '
 
 
-make_attrs_list <- function(node) {
-  attrs <- xml2::xml_attrs(node)
-  purrr::imap(attrs, \(.x, i){
-    paste0(i, ' = ', dplyr::if_else(.x == "", "NA", glue::glue('"{.x}"')))
-  })
-}
-
-render_node <- function(node, indent = 0, prefix = FALSE) {
-	if (xml2::xml_name(node) == "text") {
-		# txt <- xml2::xml_text(node)
-		# if (nchar(trimws(txt)) > 0) {
-		# 	paste0('"', trimws(txt), '"')
-		# }
-	  render_text(node)
-	} else {
-		tagName <- if (prefix) paste0("tags$", xml2::xml_name(node)) else xml2::xml_name(node)
-		newIndent <- indent + length(tagName) + 1
-		xml2::xml_contents(node) %>%
-		  purrr::map(render_node, indent = newIndent, prefix = prefix) %>%
-		  purrr::compact() %>%
-			append(make_attrs_list(node), after = 0) %>%
-			paste(collapse = stringr::str_pad(",\n", width = newIndent, side = c("right"))) %>%
-			trimws(which = c("left")) %>%
-			paste0(tagName, "(", ., ")")
-		  # identity()
-	}
-}
-
-render_text <- function(node) {
-  txt <- xml2::xml_text(node)
-  if (nchar(trimws(txt)) > 0) {
-    paste0('"', trimws(txt), '"')
-  }
-}
-
-
-html2R <- function(htmlStr, prefix = FALSE) {
-	htmlStr %>%
-    xml2::read_xml(tree) %>%
-    xml2::xml_find_first(".") %>%
-		render_node(prefix = prefix)
-}
-
-'<div>Hello</div>' |>
-  xml2::read_html() |>
-  xml2::xml_find_all("./body/*") |>
-  purrr::map(xml2::xml_contents)
-
 get_nodeset_from_string <- function(str) {
   str |>
     xml2::read_html() |>
@@ -137,30 +89,38 @@ get_node_params <- function(node) {
   }
 }
 
-node_params_to_str <- function(node_params) {
-  if (node_params$name == "text") {
-    node_params$contents
-  } else {
-    tag_name <- glue::glue("htmltools::tags${node_params$name}")
-    params_collapsed <- attrs_to_params(node_params$attrs)
-    contents <- node_params$contents
-    if (length(contents) > 0) {
-      contents <- contents |>
-        purrr::map_chr(node_params_to_str)
-      contents <- paste0(", ", contents)
-    } else {
-      contents <- ""
-    }
-    glue::glue("{tag_name}({params_collapsed}{contents})")
-  }
-}
-
 attrs_to_params <- function(attrs) {
   if (length(attrs) == 0) return("")
   params_names <- names(attrs)
   params_values <- unname(attrs)
   params <- glue::glue("`{params_names}` = \"{params_values}\"")
   glue::glue_collapse(params, ", ")
+}
+
+node_params_to_str <- function(node_params) {
+  if (node_params$name == "text") {
+    paste0("'", node_params$contents, "'")
+  } else {
+    tag_name <- glue::glue("htmltools::tags${node_params$name}")
+    params <- attrs_to_params(node_params$attrs)
+    contents <- node_params$contents
+    if (length(contents) > 0) {
+      contents <- contents |>
+        purrr::map_chr(node_params_to_str) |>
+        glue::glue_collapse(sep = ", ")
+      # contents <- paste0(", ", contents)
+    } else {
+      contents <- ""
+    }
+    fun_args <- c(params, contents)
+    fun_args <- fun_args[fun_args != ""]
+    fun_args <- paste(fun_args, collapse = ", ")
+    glue::glue("{tag_name}({fun_args})")
+  }
+}
+
+into_taglist <- function(tags) {
+  glue::glue("htmltools::tagList({tags})")
 }
 
 fake_nodeset <- list()
@@ -178,6 +138,11 @@ list(
           name = "div",
           attrs = character(),
           contents = fake_nodeset
+        ),
+        list(
+          name = "div",
+          attrs = character(),
+          contents = fake_nodeset
         )
       )
     )
@@ -185,16 +150,29 @@ list(
 ) |>
   node_params_to_str()
 
-test_html_2 |>
-  get_nodeset_from_string() |>
-  purrr::map(get_node_params) |>
-  node_params_to_str()
-  identity()
+html_to_r <- function(html) {
+  html |>
+    get_nodeset_from_string() |>
+    purrr::map(get_node_params) |>
+    purrr::map_chr(node_params_to_str) |>
+    glue::glue_collapse(sep = ", ") |>
+    into_taglist() |>
+    styler::style_text()
+}
+
+test_html_1 |> html_to_r()
+
+html_to_taglist <- function(html) {
+  html |>
+    html_to_r() |>
+    parse(text = _) |>
+    eval()
+}
 
 ui <- fluidPage(
   titlePanel("HTML to R Converter"),
   fluidRow(
-    column(5, textAreaInput("html", "HTML", rows=20, value = test_html)
+    column(5, textAreaInput("html", "HTML", rows=20, value = test_html_1)
     ),
     column(2, checkboxInput("prefix", "Prefix"), actionButton("convert", "Convert")),
     column(5, tags$pre(textOutput("rCode")))
@@ -205,7 +183,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
 
 	rcode <- eventReactive(input$convert, {
-		html2R(input$html, prefix = input$prefix)
+	  html_to_r(input$html)
 	}, ignoreInit = TRUE)
 
 	output$rCode <- renderText(rcode())
