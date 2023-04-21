35e5bf2f570ff48205b574b0b79044e852171c4b
calderonsamuel <samuel.calderon@uarm.pe>
2023-04-21 12:22:13 -0500

    copy to clipboard button works an looks as expected

--- inst/js/copyToClipboard.js
+++ inst/js/copyToClipboard.js
@@ -1,14 +1,36 @@
 $('pre').each(function() {
-  const $codeChunk = $(this);
-  const $copyButton = $('<button>').text('Copy');
-  $codeChunk.append($copyButton);
+  const $codeChunk = $(this).css({
+    "border-radius": "0 0 5px 5px" // rounded at bottom
+  });
 
-/*
+  let $language = $codeChunk.children("code").attr("class")
+  if (typeof $language === "undefined") {
+    $language = "output"
+  }
+  const $languageP = $('<p>').text($language).addClass("px-2 py-1 m-0 text-muted small")
+
+  const $copyButton = $('<button>')
+    .text('Copy')
+    .addClass('btn btn-secondary btn-sm')
+    .css({
+      "border-radius": "0 5px 0 0" // rounded at top right
+    });
+
+  const $buttonDiv = $('<div>')
+    .addClass("d-flex justify-content-between bg-dark")
+    .append($languageP)
+    .append($copyButton)
+    .css({
+      "border-radius": "5px 5px 0 0" // rounded at top
+    })
+
+  $codeChunk.before($buttonDiv);
+
+
   $copyButton.on('click', function() {
     const codeText = $codeChunk.text();
     navigator.clipboard.writeText(codeText);
   });
 
-*/
-});
 
+});
