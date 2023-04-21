7dc82d6e358db09c03f8941fb34c01f5b58140db
calderonsamuel <samuel.calderon@uarm.pe>
2023-04-21 09:55:57 -0500

    incorporate html_to_taglist into package

---
+++ R/html_to_taglist.R
@@ -0,0 +1,159 @@
+#' Convert HTML to a Tag List
+#'
+#' This function takes a character string of HTML code and returns a tag list that can be used to
+#' display the HTML content in an R Markdown document or Shiny app. The resulting tag list can be
+#' passed as an argument to the `htmltools::tagQuery()` function or used as an input to other HTML
+#' rendering functions in R.
+#'
+#' @param html A character string of HTML code
+#' @return A tag list that can be used to display the HTML content
+#' @export
+html_to_taglist <- function(html) {
+  html %>%
+    html_to_r() %>%
+    parse(text = .) %>%
+    eval()
+}
+
+
+#' Convert HTML to R Code
+#'
+#' This function takes a character string of HTML code and returns a styled R code that can be used
+#' to recreate the HTML structure. The resulting R code is a character string that can be copied and
+#' pasted into an R script or console.
+#'
+#' @param html A character string of HTML code
+#' @return A character string of styled R code that can be used to recreate the HTML structure
+#' @export
+html_to_r <- function(html) {
+  html %>%
+    html_str_to_nodeset() %>%
+    purrr::map(get_node_params) %>%
+    purrr::map_chr(node_params_to_str) %>%
+    glue::glue_collapse(sep = ", ") %>%
+    into_taglist()
+}
+
+#' HTML string to xml nodeset
+#'
+#' This function takes HTML defined as a string and returns it as a xml_nodeset.
+#'
+#' @param str A character string that represents the HTML to be parsed
+#' @return A nodeset representing the parsed HTML
+html_str_to_nodeset <- function(str) {
+  str %>%
+    xml2::read_html() %>%
+    xml2::xml_find_all("./body/*")
+}
+
+
+node_is_text <- function(node) xml2::xml_name(node) == "text"
+
+node_text_is_empty <- function(node) xml2::xml_text(node, trim = TRUE) == ""
+
+node_content_is_nodeset <- function(node) "xml_nodeset" %in% class(node$contents)
+
+node_content_is_empty <- function(node) length(node$content) == 0
+
+#' Get Nodeset Tag Contents
+#'
+#' This function takes a nodeset and returns the contents of each tag.
+#'
+#' @param nodeset A nodeset representing a parsed HTML document
+#' @return A character vector containing the contents of each tag in the nodeset
+get_nodeset_tag_contents <- function(nodeset) {
+  nodeset %>%
+    xml2::xml_contents() %>%
+    purrr::discard(\(node) node_is_text(node) && node_text_is_empty(node))
+}
+
+
+#' Get Node Parameters
+#'
+#' This function takes a node and returns a list with its name, attributes, and contents.
+#' This functions applies recursively to every element of its contents until the element is plain text or has no extra content.
+#'
+#' @param node A node representing an element or text node in a parsed HTML document
+#' @return A list with the name, attributes, and contents of the node
+#'
+get_node_params <- function(node) {
+  if (node_is_text(node)) {
+    list(
+      name = "text",
+      attrs = xml2::xml_attrs(node),
+      contents = xml2::xml_text(node)
+    )
+  } else {
+    node_with_params <- list(
+      name = xml2::xml_name(node),
+      attrs = xml2::xml_attrs(node),
+      contents = get_nodeset_tag_contents(node)
+    )
+    if (node_content_is_nodeset(node_with_params) && !node_content_is_empty(node_with_params)) {
+      node_with_params$contents <- node_with_params$contents %>%
+        purrr::map(get_node_params)
+    }
+    node_with_params
+  }
+}
+
+
+#' Convert Attributes to Parameters
+#'
+#' This function takes a named character vector representing attributes and returns a character string
+#' that can be used as a parameter list in an HTML tag.
+#'
+#' @param attrs A named character vector representing attributes
+#' @return A character string that can be used as a parameter list in an HTML tag
+attrs_to_params <- function(attrs) {
+  if (length(attrs) == 0) return("")
+  params_names <- names(attrs)
+  params_values <- unname(attrs)
+  params <- glue::glue("`{params_names}` = \"{params_values}\"")
+  glue::glue_collapse(params, ", ")
+}
+
+
+#' Convert Node Parameters to String
+#'
+#' This function takes a list of parameters for an HTML tag and returns a character string that
+#' represents the tag with the given parameters. Aplies recursively to every child content until
+#' content is text or empty.
+#'
+#' @param node_params A list of parameters for an HTML tag
+#' @return A character string that represents the tag with the given parameters
+#'
+node_params_to_str <- function(node_params) {
+  if (node_params$name == "text") {
+    paste0("'", node_params$contents, "'")
+  } else {
+    tag_name <- glue::glue("htmltools::tags${node_params$name}")
+    params <- attrs_to_params(node_params$attrs)
+    contents <- node_params$contents
+    if (length(contents) > 0) {
+      contents <- contents %>%
+        purrr::map_chr(node_params_to_str) %>%
+        glue::glue_collapse(sep = ", ")
+      # contents <- paste0(", ", contents)
+    } else {
+      contents <- ""
+    }
+    fun_args <- c(params, contents)
+    fun_args <- fun_args[fun_args != ""]
+    fun_args <- paste(fun_args, collapse = ", ")
+    glue::glue("{tag_name}({fun_args})")
+  }
+}
+
+
+#' Paste tags string inside a tagList
+#'
+#' This function takes a list of HTML tags and returns a character string that, when evaluated,
+#' will produce a tagList object containing the given tags.
+#'
+#' @param tags_str A list of HTML tags
+#' @return A character string that, when evaluated, will produce a tagList object containing the given tags
+into_taglist <- function(tags_str) {
+  glue::glue("htmltools::tagList({tags_str})")
+}
+
--- inst/test_scripts/html_to_shinytag_app.R
+++ inst/test_scripts/html_to_shinytag_app.R
@@ -1,8 +1,8 @@
-library(magrittr)
+library(shiny)
+library(gptstudio)
 
-test_html_1 <-
-	'
-  <div>
+test_html <-
+  '
     <table style="width:100%">
   	  <tr>
   	    <th>Firstname</th>
@@ -14,165 +14,20 @@ test_html_1 <-
   	    <td>Smith</td>
   	    <td>50</td>
   	  </tr>
-  	  <tr>
-  	    <td>Eve</td>
-  	    <td>Jackson</td>
-  	    <td>94</td>
-  	  </tr>
   	</table>
-  </div>
   '
-test_html_2 <-
-	'
-  <div class="bs-dropdown" data-toggle>
-  <img src="here">
-  <div>Hello</div>
-    <table style="width:100%">
-  	  <tr>
-  	    <th>Firstname</th>
-  	    <th>Lastname</th>
-  	    <th>Age</th>
-  	  </tr>
-  	  <tr>
-  	    <td>Jill</td>
-  	    <td>Smith</td>
-  	    <td>50</td>
-  	  </tr>
-  	  <tr>
-  	    <td>Eve</td>
-  	    <td>Jackson</td>
-  	    <td>94</td>
-  	  </tr>
-  	</table>
-  </div>
-  '
-
-
-get_nodeset_from_string <- function(str) {
-  str |>
-    xml2::read_html() |>
-    xml2::xml_find_all("./body/*")
-}
-
-node_is_text <- function(node) xml2::xml_name(node) == "text"
-
-node_text_is_empty <- function(node) xml2::xml_text(node, trim = TRUE) == ""
-
-content_is_nodeset <- function(node) "xml_nodeset" %in% class(node$contents)
-
-content_is_empty <- function(node) length(node$content) == 0
-
-get_nodeset_tag_contents <- function(nodeset) {
-  nodeset |>
-    xml2::xml_contents() |>
-    purrr::discard(\(node) node_is_text(node) && node_text_is_empty(node))
-}
-
-get_node_params <- function(node) {
-  if (node_is_text(node)) {
-    list(
-      name = "text",
-      attrs = xml2::xml_attrs(node),
-      contents = xml2::xml_text(node)
-    )
-  } else {
-    node_with_params <- list(
-      name = xml2::xml_name(node),
-      attrs = xml2::xml_attrs(node),
-      contents = get_nodeset_tag_contents(node)
-    )
-    if (content_is_nodeset(node_with_params) && !content_is_empty(node_with_params)) {
-      node_with_params$contents <- node_with_params$contents |>
-        purrr::map(get_node_params)
-    }
-    node_with_params
-  }
-}
-
-attrs_to_params <- function(attrs) {
-  if (length(attrs) == 0) return("")
-  params_names <- names(attrs)
-  params_values <- unname(attrs)
-  params <- glue::glue("`{params_names}` = \"{params_values}\"")
-  glue::glue_collapse(params, ", ")
-}
-
-node_params_to_str <- function(node_params) {
-  if (node_params$name == "text") {
-    paste0("'", node_params$contents, "'")
-  } else {
-    tag_name <- glue::glue("htmltools::tags${node_params$name}")
-    params <- attrs_to_params(node_params$attrs)
-    contents <- node_params$contents
-    if (length(contents) > 0) {
-      contents <- contents |>
-        purrr::map_chr(node_params_to_str) |>
-        glue::glue_collapse(sep = ", ")
-      # contents <- paste0(", ", contents)
-    } else {
-      contents <- ""
-    }
-    fun_args <- c(params, contents)
-    fun_args <- fun_args[fun_args != ""]
-    fun_args <- paste(fun_args, collapse = ", ")
-    glue::glue("{tag_name}({fun_args})")
-  }
-}
-
-into_taglist <- function(tags) {
-  glue::glue("htmltools::tagList({tags})")
-}
-
-fake_nodeset <- list()
-class(fake_nodeset) <- c("xml_nodeset", class(fake_nodeset))
-
-list(
-  name = "div",
-  attrs = c(class = "card"),
-  contents = list(
-    list(
-      name = "div",
-      attrs = c(class = "card-header", "data-toggle" = "yes"),
-      contents = list(
-        list(
-          name = "div",
-          attrs = character(),
-          contents = fake_nodeset
-        ),
-        list(
-          name = "div",
-          attrs = character(),
-          contents = fake_nodeset
-        )
-      )
-    )
-  )
-) |>
-  node_params_to_str()
-
-html_to_r <- function(html) {
-  html |>
-    get_nodeset_from_string() |>
-    purrr::map(get_node_params) |>
-    purrr::map_chr(node_params_to_str) |>
-    glue::glue_collapse(sep = ", ") |>
-    into_taglist() |>
+# this works in console but not in app
+format_r_code_str <- function(code_str){
+  code_str |>
+    stringr::str_replace_all("(\\()([a-z[:symbol:]])", "\\1\n\\2") |> # new line after parenthesis or opening backtick
+    stringr::str_replace_all("(,)", "\\1\n") |> # newline after comma
     styler::style_text()
-}
-
-test_html_1 |> html_to_r()
-
-html_to_taglist <- function(html) {
-  html |>
-    html_to_r() |>
-    parse(text = _) |>
-    eval()
 }
 
 ui <- fluidPage(
   titlePanel("HTML to R Converter"),
   fluidRow(
-    column(5, textAreaInput("html", "HTML", rows=20, value = test_html_1)
+    column(5, textAreaInput("html", "HTML", rows=20, value = test_html)
     ),
     column(2, checkboxInput("prefix", "Prefix"), actionButton("convert", "Convert")),
     column(5, tags$pre(textOutput("rCode")))
@@ -183,7 +38,9 @@ ui <- fluidPage(
 server <- function(input, output, session) {
 
 	rcode <- eventReactive(input$convert, {
-	  html_to_r(input$html)
+	  input$html |>
+	    html_to_r() |>
+	    format_r_code_str()
 	}, ignoreInit = TRUE)
 
 	output$rCode <- renderText(rcode())
