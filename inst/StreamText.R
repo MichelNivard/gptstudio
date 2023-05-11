0e84a59b968fc86b35f9a05b51e4cd15d6b0a40e
Samuel Calderon <samuel.calderon@uarm.pe>
2023-05-11 17:01:23 -0500

    add methods to handle base chunk and generate chunk list

--- inst/StreamText.R
+++ inst/StreamText.R
@@ -3,18 +3,17 @@ StreamText <- R6::R6Class(
   public = list(
 
     initialize = function() {
-      private$value <- ""
+      private$value <- "" # this will hold the stream and mutate it with inner methods
+      private$value_buffer <- private$value # this will hold previous and current value
+      private$full_response <- private$value # this will hold the full response
       invisible(self)
     },
 
-    append_text = function(text) {
+    handle_text_stream = function(text) {
       private$value <- paste0(private$value, text)
+      private$full_response <- paste0(private$full_response, text)
 
-      # never change this order unless the API itself changes
-      private$handle_chunk_id()
-      private$handle_chunk_object()
-      private$handle_chunk_created()
-      private$handle_chunk_model()
+      private$generate_chunk_list()
 
       invisible(self)
     },
@@ -28,15 +27,42 @@ StreamText <- R6::R6Class(
       private$value
     },
 
-    get_chunk_base = function() {
-      private$chunk
+    get_full_response = function() {
+      private$full_response
+    },
+
+    get_base_chunk = function() {
+      private$base_chunk
+    },
+
+    get_chunk_list = function() {
+      private$chunk_list
+    },
+
+    reset_chunk_setup = function() {
+      private$value = NULL
+
+      private$base_chunk <- list(
+        choices = NULL,
+        created = NULL,
+        id = NULL,
+        model = NULL,
+        object = NULL
+      )
+
+      private$chunk_list <- NULL
+
+      invisible(self)
     }
 
   ),
 
   private = list(
     value = NULL,
-    chunk = list(
+    value_buffer = NULL,
+    full_response = NULL,
+
+    base_chunk = list(
       choices = NULL,
       created = NULL,
       id = NULL,
@@ -44,59 +70,143 @@ StreamText <- R6::R6Class(
       object = NULL
     ),
 
-    handle_chunk_id = function() {
-      is_set <- !is.null(private$chunk$id)
+    chunk_list = NULL,
+
+    handle_base_chunk_id = function() {
+      is_set <- !is.null(private$base_chunk$id)
 
       if (is_set) return(NULL)
 
+      # matches strings that start with the exact sequence: `{"id":"chatcmpl-` followed by one or more alphanumeric characters, and ending with a comma.
       if (stringr::str_detect(private$value, "^\\{\"id\":\"chatcmpl-[a-zA-Z0-9]+\",")) {
 
-        private$chunk$id <- stringr::str_extract(private$value, "chatcmpl-[a-zA-Z0-9]+")
+        private$base_chunk$id <- stringr::str_extract(private$value, "chatcmpl-[a-zA-Z0-9]+")
 
         private$value <- stringr::str_replace(private$value, "^(\\{)\"id\":\"chatcmpl-[a-zA-Z0-9]+\",", "\\1")
       }
     },
 
-    handle_chunk_object = function() {
-      is_set <- !is.null(private$chunk$object)
+    handle_base_chunk_object = function() {
+      is_set <- !is.null(private$base_chunk$object)
 
       if (is_set) return(NULL)
 
+      # matches strings that start with the exact sequence: {"object":" followed by one or more word characters or dots, and ending with a comma.
       if (stringr::str_detect(private$value, "^\\{\"object\":\"[\\w\\.]+\",")) {
 
-        # private$chunk$object <- stringr::str_replace(private$value, "^\\{\"object\":\"([\\w\\.]+).*", "\\1")
-        private$chunk$object <- "chat.completion.chunk"
+        # private$base_chunk$object <- stringr::str_replace(private$value, "^\\{\"object\":\"([\\w\\.]+).*", "\\1")
+        private$base_chunk$object <- "chat.completion.chunk"
 
         private$value <- stringr::str_replace(private$value, "^(\\{)\"object\":\"[\\w\\.]+\",", "\\1")
       }
     },
 
-    handle_chunk_created = function() {
-      is_set <- !is.null(private$chunk$created)
+    handle_base_chunk_created = function() {
+      is_set <- !is.null(private$base_chunk$created)
 
       if (is_set) return(NULL)
 
+      # matches strings that start with the exact sequence: {"created": followed by one or more digits, and ending with a comma.
       if (stringr::str_detect(private$value, "^\\{\"created\":[\\d]+,")) {
 
-        private$chunk$created <- stringr::str_replace(private$value, "^\\{\"created\":([\\d]+).*", "\\1") |> as.integer()
+        private$base_chunk$created <- stringr::str_replace(private$value, "^\\{\"created\":([\\d]+).*", "\\1") |> as.integer()
 
         private$value <- stringr::str_replace(private$value, "^(\\{)\"created\":[\\d]+,", "\\1")
       }
     },
 
-    handle_chunk_model = function() {
-      is_set <- !is.null(private$chunk$model)
+    handle_base_chunk_model = function() {
+      is_set <- !is.null(private$base_chunk$model)
 
       if (is_set) return(NULL)
 
+      # matches strings that start with the exact sequence: {"model":" followed by one or more word characters, digits, dots, or hyphens, and ending with a comma.
       if (stringr::str_detect(private$value, "^\\{\"model\":\"[\\w\\d\\.\\-]+\",")) {
 
-        private$chunk$model <- stringr::str_replace(private$value, "^\\{\"model\":\"([\\w\\d\\.\\-]+).*", "\\1")
+        private$base_chunk$model <- stringr::str_replace(private$value, "^\\{\"model\":\"([\\w\\d\\.\\-]+).*", "\\1")
 
         private$value <- stringr::str_replace(private$value, "^(\\{)\"model\":\"[\\w\\d\\.\\-]+\",", "\\1")
       }
 
+    },
+
+    initialize_chunk_list = function() {
+      base_chunk_tracks_choices <- !is.null(private$base_chunk$choices)
+
+      if (base_chunk_tracks_choices) return(NULL)
+
+      usage_regex <- "^\\{\"usage\":\\{\"prompt_tokens\":\\d+,\"completion_tokens\":\\d+,\"total_tokens\":\\d+\\},"
+      choices_regex <- "\"choices\":\\[\\{\"message\":\\{\"role\":\"assistant\",\"content\":\""
+
+      full_regex <- paste0(usage_regex, choices_regex)
+
+      if (stringr::str_detect(private$value, full_regex)) {
+        private$value <- stringr::str_replace(private$value, full_regex, "")
+
+        private$base_chunk$choices <- list(
+          list(
+            delta = list(),
+            finish_reason = NULL,
+            index = 0L
+          )
+        )
+
+        role_chunk <- private$generate_single_chunk(
+          delta_name = "role",
+          delta_value = "assistant"
+        )
+
+        private$chunk_list <- list(role_chunk)
+      }
+
+    },
+
+    generate_chunk_list = function() {
+      # never change this order unless the API itself changes
+      private$handle_base_chunk_id()
+      private$handle_base_chunk_object()
+      private$handle_base_chunk_created()
+      private$handle_base_chunk_model()
+
+      private$initialize_chunk_list()
+
+      private$append_new_chunk()
+    },
+
+    generate_single_chunk = function(delta_name, delta_value) {
+      match.arg(delta_name, c("role", "content"))
+
+      copied_chunk <- private$base_chunk
+
+      delta_value_has_new_line <- stringr::str_detect(delta_value, "\\n") # basically detecs the last stream
+
+      if (delta_value_has_new_line) {
+        copied_chunk$choices[[1]]$finish_reason <- "stop"
+        return(copied_chunk)
+      }
+
+      copied_chunk$choices[[1]]$delta[[delta_name]] <- delta_value
+
+      copied_chunk
+
+    },
+
+    append_new_chunk = function() {
+      chunk_list_is_null <- is.null(private$chunk_list)
+
+      if (chunk_list_is_null) return(NULL)
+
+      new_chunk <- private$generate_single_chunk(
+        delta_name = "content",
+        delta_value = private$value
+      )
+
+      private$chunk_list <- c(private$chunk_list, list(new_chunk))
+
+      # reset value
+      private$value <- ""
     }
+
   )
 )
 
@@ -106,35 +216,168 @@ full_stream |>
   stringr::str_replace("^(\\{)\"id\":\"chatcmpl-[a-zA-Z0-9]+\",", "\\1") |>
   stringr::str_replace("^(\\{)\"object\":\"[\\w\\.]+\",", "\\1") |>
   stringr::str_replace("^(\\{)\"created\":[\\d]+,", "\\1") |>
-  stringr::str_replace("^(\\{)\"model\":\"[\\w\\d\\.\\-]+\",", "\\1")
+  stringr::str_replace("^(\\{)\"model\":\"[\\w\\d\\.\\-]+\",", "\\1") |>
+  stringr::str_replace(full_regex, "")
 
-# tempchar <- StreamText$new()
-#
-# handle_stream <- function(x, char) {
-#   parsed <- rawToChar(x)
-#
-#   # print(parsed)
-#
-#   char$append_text(parsed)
-#
-#   return(TRUE)
-# }
-#
-# request_base("chat/completions") |>
-#   httr2::req_body_json(data = list(
-#     model = "gpt-3.5-turbo",
-#     messages = list(
-#       list(
-#         role = "user",
-#         content = "Count from 1 to 20"
-#       )
-#     )
-#   )) |>
-#   httr2::req_stream(callback = \(x) handle_stream(x, tempchar), buffer_kb = 10/1024)
+tempchar <- character()
+
+handle_stream <- function(x, char) {
+  parsed <- rawToChar(x)
+
+  # print(parsed)
+
+  # char$handle_text_stream(parsed)
+
+  tempchar <<- c(tempchar, parsed)
+
+  return(TRUE)
+}
+
+request_base("chat/completions") |>
+  httr2::req_body_json(data = list(
+    model = "gpt-3.5-turbo",
+    messages = list(
+      list(
+        role = "user",
+        content = "Generate a JSON text to store customer data. return a single object for 3 customers."
+      )
+    )
+  )) |>
+  httr2::req_stream(callback = \(x) handle_stream(x, tempchar), buffer_kb = 10/1024)
+
+example_stream2 <-
+  c(
+    "{\"id\":\"cha",
+    "tcmpl-7F7B",
+    "sNWFht4pWw",
+    "rPPSE3vJAu",
+    "B9efW\",\"ob",
+    "ject\":\"cha",
+    "t.completi",
+    "on\",\"creat",
+    "ed\":168383",
+    "6916,\"mode",
+    "l\":\"gpt-3.",
+    "5-turbo-03",
+    "01\",\"usage",
+    "\":{\"prompt",
+    "_tokens\":2",
+    "6,\"complet",
+    "ion_tokens",
+    "\":251,\"tot",
+    "al_tokens\"",
+    ":277},\"cho",
+    "ices\":[{\"m",
+    "essage\":{\"",
+    "role\":\"ass",
+    "istant\",\"c",
+    "ontent\":\"{",
+    "\\n  \\\"cust",
+    "omers\\\": [",
+    "\\n    {\\n ",
+    "     \\\"fir",
+    "stName\\\": ",
+    "\\\"John\\\",\\",
+    "n      \\\"l",
+    "astName\\\":",
+    " \\\"Doe\\\",\\",
+    "n      \\\"e",
+    "mail\\\": \\\"",
+    "johndoe@ex",
+    "ample.com\\",
+    "\",\\n      ",
+    "\\\"phone\\\":",
+    " \\\"555-555",
+    "-5555\\\",\\n",
+    "      \\\"ad",
+    "dress\\\": {",
+    "\\n        ",
+    "\\\"street\\\"",
+    ": \\\"123 Ma",
+    "in St\\\",\\n",
+    "        \\\"",
+    "city\\\": \\\"",
+    "Anytown\\\",",
+    "\\n        ",
+    "\\\"state\\\":",
+    " \\\"CA\\\",\\n",
+    "        \\\"",
+    "zip\\\": \\\"1",
+    "2345\\\"\\n  ",
+    "    }\\n   ",
+    " },\\n    {",
+    "\\n      \\\"",
+    "firstName\\",
+    "\": \\\"Jane\\",
+    "\",\\n      ",
+    "\\\"lastName",
+    "\\\": \\\"Doe\\",
+    "\",\\n      ",
+    "\\\"email\\\":",
+    " \\\"janedoe",
+    "@example.c",
+    "om\\\",\\n   ",
+    "   \\\"phone",
+    "\\\": \\\"555-",
+    "555-5555\\\"",
+    ",\\n      \\",
+    "\"address\\\"",
+    ": {\\n     ",
+    "   \\\"stree",
+    "t\\\": \\\"456",
+    " Elm St\\\",",
+    "\\n        ",
+    "\\\"city\\\": ",
+    "\\\"Anytown\\",
+    "\",\\n      ",
+    "  \\\"state\\",
+    "\": \\\"CA\\\",",
+    "\\n        ",
+    "\\\"zip\\\": \\",
+    "\"12345\\\"\\n",
+    "      }\\n ",
+    "   },\\n   ",
+    " {\\n      ",
+    "\\\"firstNam",
+    "e\\\": \\\"Bob",
+    "\\\",\\n     ",
+    " \\\"lastNam",
+    "e\\\": \\\"Smi",
+    "th\\\",\\n   ",
+    "   \\\"email",
+    "\\\": \\\"bobs",
+    "mith@examp",
+    "le.com\\\",\\",
+    "n      \\\"p",
+    "hone\\\": \\\"",
+    "555-555-55",
+    "55\\\",\\n   ",
+    "   \\\"addre",
+    "ss\\\": {\\n ",
+    "       \\\"s",
+    "treet\\\": \\",
+    "\"789 Oak S",
+    "t\\\",\\n    ",
+    "    \\\"city",
+    "\\\": \\\"Anyt",
+    "own\\\",\\n  ",
+    "      \\\"st",
+    "ate\\\": \\\"C",
+    "A\\\",\\n    ",
+    "    \\\"zip\\",
+    "\": \\\"12345",
+    "\\\"\\n      ",
+    "}\\n    }\\n",
+    "  ]\\n}\"},\"",
+    "finish_rea",
+    "son\":\"stop",
+    "\",\"index\":",
+    "0}]}\n"
+  )
 
 
 
-example_stream <- c(
+example_stream1 <- c(
   "{\"id\":\"cha",
   "tcmpl-7F2E",
   "GwP25x19nS",
@@ -176,8 +419,10 @@ example_stream <- c(
 
 tempchar_example <- StreamText$new()
 
-example_stream |>
-  purrr::walk(\(x) tempchar_example$append_text(x))
+example_stream2 |>
+  purrr::walk(\(x) tempchar_example$handle_text_stream(x))
 
-tempchar_example$get_chunk_base()
+tempchar_example$get_base_chunk()
 tempchar_example$get_value()
+tempchar_example$get_full_response()
+tempchar_example$get_chunk_list()
