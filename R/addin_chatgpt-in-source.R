0e9fc80feca26bf164d44e84810a74faccdcc268
calderonsamuel <samuel.calderon@uarm.pe>
2023-05-21 19:59:55 -0500

    fix #95

--- R/addin_chatgpt-in-source.R
+++ R/addin_chatgpt-in-source.R
@@ -14,7 +14,6 @@
 #' addin_chatgpt_in_source()
 #' }
 addin_chatgpt_in_source <- function() {
-  cli_inform(c("i" = "Sending query to ChatGPT..."))
   gpt_chat_in_source(
     style = getOption("gptstudio.code_style"),
     skill = getOption("gptstudio.skill")
