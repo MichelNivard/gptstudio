9079a6e785d94339ffebbdf6c6d761199220a04d
calderonsamuel <samuel.calderon@uarm.pe>
2023-05-24 13:35:24 -0500

    add translation infrastructure

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,15 @@
 .onLoad <- function(lib, pkg) {
+  env_language <- Sys.getenv("GPTSTUDIO_LANGUAGE")
+
   op <- options()
+
   op_gptstudio <- list(
     gptstudio.valid_api  = FALSE,
     gptstudio.openai_key = NULL,
     gptstudio.max_tokens = 500,
     gptstudio.code_style = "no preference",
-    gptstudio.skill      = "beginner"
+    gptstudio.skill      = "beginner",
+    gptstudio.language = if (env_language == "") "en" else (env_language)
   )
   toset <- !(names(op_gptstudio) %in% names(op))
   if (any(toset)) options(op_gptstudio[toset])
