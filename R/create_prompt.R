0039bd3b3bf0bfd49f98c65ac3e47cb38a5603fa
James Wade <github@jameshwade.com>
2024-01-28 15:13:28 -0500

    bug: allow NULL for style, code, and task

--- R/create_prompt.R
+++ R/create_prompt.R
@@ -1,64 +1,100 @@
 #' Create system prompt
 #'
-#' This creates a system prompt based on the user defined parameters.
+#' This function creates a customizable system prompt based on user-defined
+#' parameters such as coding style, skill level, and task. It supports
+#' customization for specific use cases through a custom prompt option.
 #'
-#' @param style A character string indicating the preferred coding style, the
-#' default is "tidyverse".
-#' @param skill The self-described skill level of the programmer,
-#' default is "beginner"
-#' @param task The task to be performed: "coding", "general", or "advanced developer".
-#' @param custom_prompt An optional custom prompt to be displayed.
-#' @param in_source Whether to add instructions to act as in a source script.
+#' @param style A character string indicating the preferred coding style. Valid
+#'   values are "tidyverse", "base", "no preference". Defaults to `getOption(gptstudio.code_style)`.
+#' @param skill The self-described skill level of the programmer. Valid values
+#'   are "beginner", "intermediate", "advanced", "genius". Defaults to `getOption(gptstudio.skill)`.
+#' @param task The specific task to be performed: "coding", "general", "advanced
+#'   developer", or "custom". This influences the generated system prompt.
+#'   Defaults to "coding".
+#' @param custom_prompt An optional custom prompt string to be utilized when
+#'   `task` is set to "custom". Default is NULL.
+#' @param in_source A logical indicating whether the instructions are intended
+#'   for use in a source script. This parameter is required and must be
+#'   explicitly set to TRUE or FALSE. Default is FALSE.
 #'
-#' @return A string
+#' @return Returns a character string that forms a system prompt tailored to the
+#'   specified parameters. The string provides guidance or instructions based on
+#'   the user's coding style, skill level, and task.
 #'
+#' @examples
+#' \dontrun{
+#' chat_create_system_prompt(in_source = TRUE)
+#' chat_create_system_prompt(style = "tidyverse",
+#'                           skill = "advanced",
+#'                           task = "coding",
+#'                           in_source = FALSE)
+#' }
 chat_create_system_prompt <-
-  function(style = c("tidyverse", "base", "no preference", NULL),
-           skill = c("beginner", "intermediate", "advanced", "genius", NULL),
-           task = c("coding", "general", "advanced developer", "custom"),
-           custom_prompt = NULL,
-           in_source) {
-    arg_match(style)
-    arg_match(skill)
-    arg_match(task)
-    assert_that(is.logical(in_source),
-                msg = "chat system prompt creation needs logical `in_source`")
+  function(style = getOption("gptstudio.code_style"),
+           skill = getOption("gptstudio.skill"),
+           task = getOption("gptstudio.task"),
+           custom_prompt = getOption("gptstudio.custom_prompt"),
+           in_source = FALSE) {
+    style %|!|%
+      rlang::arg_match(style,
+                       values = c("tidyverse", "base", "no preference"))
+    skill %|!|%
+      rlang::arg_match(skill,
+                       values = c("beginner", "intermediate", "advanced", "genius"))
+    task %|!|%
+      rlang::arg_match(
+        task,
+        c("coding", "general", "advanced developer", "custom"))
 
-    if (!is.null(custom_prompt) && task == "custom") {
+    # Custom prompt bypass
+    if (!is.null(custom_prompt) && task == "custom" && custom_prompt != "") {
       return(custom_prompt)
     }
 
-    if (task == "general") {
-      return("You are a helpful chat assistant.")
+    task %|!|%
+      switch(task,
+             "general" = return("You are a helpful chat assistant."),
+             "advanced developer" = return("Write code only. Fewer instructions and comments."))
+
+    if (is.null(c(skill, style))) {
+      intro <-
+        glue::glue(
+          "You are a chat bot assisting an R programmer working in RStudio."
+        )
+    } else {
+      intro <-
+        glue::glue(
+          "As a chat bot assisting an R programmer working in the RStudio IDE",
+          "it is important to tailor responses to their skill level and",
+          "preferred coding style.",
+          .sep = " "
+        )
     }
 
-    if (task == "advanced developer") {
-      "Write code only. Fewer instructions and comments."
-    }
+    # Crafting the message parts based on provided arguments
 
-    # nolint start
+    about_skill <- if (!is.null(skill)) {
+      glue::glue("They consider themselves to be a {skill} R programmer.",
+                 "Provide answers with their skill level in mind.",
+                 .sep = " ")
+    } else ""
 
-    intro <- "As a chat bot assisting an R programmer working in the RStudio IDE, it is important to tailor responses to their skill level and preferred coding style."
-    about_skill <- glue(
-      "They consider themselves to be a {skill} R programmer. Provide answers with their skill level in mind."
-    )
-
-    about_style <-
+    about_style <- if (!is.null(style)) {
       switch(style,
              "no preference" = "",
-             "base" = "They prefer to use a base R style of coding. When possible, answer code questions using base R rather than the tidyverse.",
+             "base" = "They prefer to use a base R style of coding. When possible, answer code questions using base R.",
              "tidyverse" = "They prefer to use a tidyverse style of coding. When possible, answer code questions using tidyverse, r-lib, and tidymodels family of packages. R for Data Science is also a good resource to pull from."
       )
+    } else ""
 
-    in_source_instructions <- if (in_source) {
-      "For any text that is not R code, write it as a code comment. Do not use code blocks or free text. Only use code and code comments."
-    } else {
-      ""
-    }
-
-    # nolint end
-
-    glue("{intro} {about_skill} {about_style} {in_source_instructions}")
+    in_source_instructions <-
+      if (in_source) {
+        glue::glue("For any text that is not R code, write it as a code",
+                   "comment. Do not use code blocks or free text. Only use",
+                   "code and code comments.",
+                   .sep = " ")
+      } else ""
+    glue::glue("{intro} {about_skill} {about_style} {in_source_instructions}")
   }
 
 
@@ -116,4 +152,12 @@ get_selection <- function() {
 insert_text <- function(improved_text) {
   rstudioapi::verifyAvailable()
   rstudioapi::insertText(improved_text)
+}
+
+`%|!|%` <- function(x, y) {
+  if (is_null(x)) x else y
+}
+
+`%||%` <- function(x, y) {
+  if (is_null(x)) y else x
 }
