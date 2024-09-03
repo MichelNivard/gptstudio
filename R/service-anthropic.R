eb83df2e06dac36d5cda9c84942a8879f04d91b3
James Wade <github@jameshwade.com>
2024-09-02 20:05:35 -0400

    streaming with req_stream_connection

--- R/service-anthropic.R
+++ R/service-anthropic.R
@@ -1,69 +1,3 @@
-#' Base for a request to the Anthropic API
-#'
-#' This function sends a request to the Anthropic API endpoint and
-#' authenticates with an API key.
-#'
-#' @param key String containing an Anthropic API key. Defaults to the
-#'   ANTHROPIC_API_KEY environmental variable if not specified.
-#' @return An httr2 request object
-request_base_anthropic <- function(key = Sys.getenv("ANTHROPIC_API_KEY")) {
-  request("https://api.anthropic.com/v1/messages") |>
-    req_headers(
-      "anthropic-version" = "2023-06-01",
-      "content-type" = "application/json",
-      "x-api-key" = key
-    )
-}
-
-query_api_anthropic <- function(request_body,
-                                key = Sys.getenv("ANTHROPIC_API_KEY"),
-                                stream = FALSE,
-                                element_callback = anthropic_handler,
-                                shiny_session = NULL,
-                                user_prompt = NULL) {
-  req <- request_base_anthropic(key) |>
-    req_body_json(data = request_body) |>
-    req_retry(max_tries = 3) |>
-    req_error(is_error = function(resp) FALSE)
-
-  if (is_true(stream)) {
-    parser <- AnthropicStreamParser$new(
-      session = shiny_session,
-      user_prompt = user_prompt
-    )
-
-    response <-
-      req |>
-      req_perform_stream(
-        callback = function(x) {
-          element <- rawToChar(x)
-          parser$parse_json(element)
-          TRUE
-        },
-        round = "line",
-        buffer_kb = 0.01
-      )
-    parser$value
-  } else {
-    response <-
-      req |>
-      req_perform()
-    # error handling
-    if (resp_is_error(response)) {
-      status <- resp_status(response) # nolint
-      description <- resp_status_desc(response) # nolint
-
-      cli::cli_abort(c(
-        "x" = "Anthropic API request failed. Error {status} - {description}",
-        "i" = "Visit the Anthropic API documentation for more details"
-      ))
-    }
-    response |>
-      resp_body_json(simplifyVector = TRUE) |>
-      purrr::pluck("content", "text")
-  }
-}
-
 #' Generate text completions using Anthropic's API
 #'
 #' @param prompt The prompt for generating completions
@@ -117,48 +51,63 @@ create_completion_anthropic <- function(prompt = list(list(role = "user", conten
   )
 }
 
-anthropic_handler <- function(x) {
-  pattern = "\\{\"type\":\"content_block_delta\",.*\\}.*\\}"
-  json <- stringr::str_extract(x, pattern)
-  if (is.na(json)) return()
-  a <- json |> jsonlite::parse_json()
-  cat(a$delta$text)
+request_base_anthropic <- function(key = Sys.getenv("ANTHROPIC_API_KEY")) {
+  request("https://api.anthropic.com/v1/messages") |>
+    req_headers(
+      "anthropic-version" = "2023-06-01",
+      "content-type" = "application/json",
+      "x-api-key" = key
+    )
 }
 
-AnthropicStreamParser <- R6::R6Class( # nolint
-  classname = "AnthropicStreamParser",
-  portable = TRUE,
-  public = list(
-    lines = NULL,
-    value = NULL,
-    shinySession = NULL,
-    user_message = NULL,
-    append_parsed_line = function(line) {
-      self$value <- paste0(self$value, line$delta$text)
-      self$lines <- c(self$lines, list(line))
-      if (!is.null(self$shinySession)) {
+query_api_anthropic <- function(request_body,
+                                 key = Sys.getenv("ANTHROPIC_API_KEY"),
+                                 stream = FALSE,
+                                 element_callback = anthropic_handler,
+                                 shiny_session = NULL,
+                                 user_prompt = NULL) {
+  req <- request_base_anthropic(key) |>
+    req_body_json(data = request_body) |>
+    req_retry(max_tries = 3) |>
+    req_error(is_error = function(resp) FALSE)
+
+  if (is_true(stream)) {
+    resp <- req_perform_connection(req, mode = "text")
+    on.exit(close(resp))
+    results <- list()
+    repeat({
+      event <- resp_stream_sse(resp)
+      if (is.null(event) || event$data == "[DONE]") {
+        break
+      }
+      json <- jsonlite::parse_json(event$data)
+      results <- merge_dicts(results, json)
+      if (!is.null(shiny_session)) {
         # any communication with JS should be handled here!!
-        self$shinySession$sendCustomMessage(
+        shiny_session$sendCustomMessage(
           type = "render-stream",
           message = list(
-            user = self$user_message,
-            assistant = shiny::markdown(self$value)
+            user = user_prompt,
+            assistant = shiny::markdown(results$delta$text)
           )
         )
+      } else {
+        cat(json$delta$text)
       }
-      invisible(self)
-    },
-    parse_json = function(x) { # nolint
-      pattern = "\\{\"type\":\"content_block_delta\",.*\\}.*\\}"
-      json <- stringr::str_extract(x, pattern)
-      if (is.na(json)) return()
-      self$append_parsed_line(json |> jsonlite::parse_json())
-      invisible(self)
-    },
-    initialize = function(session = NULL, user_prompt = NULL) {
-      self$lines <- list()
-      self$shinySession <- session
-      self$user_message <- shiny::markdown(user_prompt)
+    })
+    invisible(results$delta$text)
+  } else {
+    response <- req |> req_perform()
+    if (resp_is_error(response)) {
+      status <- resp_status(response)
+      description <- resp_status_desc(response)
+      cli::cli_abort(c(
+        "x" = "Anthropic API request failed. Error {status} - {description}",
+        "i" = "Visit the Anthropic API documentation for more details"
+      ))
     }
-  )
-)
+    response |>
+      resp_body_json(simplifyVector = TRUE) |>
+      purrr::pluck("content", "text")
+  }
+}
