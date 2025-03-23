d105212cf36dc1704c101918494da6e49489987e
Samuel Calderon <samuel.calderon@uarm.pe>
2025-03-23 10:50:05 -0500

    add test for `chat(service="openai")`

---
+++ tests/testthat/test-chat.R
@@ -0,0 +1,30 @@
+describe("chat() openai", {
+  skip_on_ci()
+  skip_on_cran()
+
+  it("fails without API KEY", {
+    expect_error({
+      withr::with_envvar(OPENAI_API_KEY = "", {
+        chat(
+          prompt = "what is 1+1",
+          service = "openai",
+          history = NULL,
+          stream = FALSE,
+          model = "gpt-4o-mini"
+        )
+      })
+    })
+  })
+
+  it ("works", {
+    expect_no_error({
+      chat(
+        prompt = "what is 1+1",
+        service = "openai",
+        history = NULL,
+        stream = FALSE,
+        model = "gpt-4o-mini"
+      )
+    })
+  })
+})
