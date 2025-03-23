74231a1a3e5a90d17f9d1e372ab6e17c18960312
Samuel Calderon <samuel.calderon@uarm.pe>
2025-03-23 11:33:15 -0500

    add test for chat(service="huggingface")

--- tests/testthat/test-chat.R
+++ tests/testthat/test-chat.R
@@ -28,3 +28,34 @@ describe("chat() openai", {
     })
   })
 })
+
+describe("chat() huggingface", {
+  skip_on_ci()
+  skip_on_cran()
+
+  it("fails without API KEY", {
+    expect_error({
+      withr::with_envvar(HF_API_KEY = "", {
+        chat(
+          prompt = "what is 1+1",
+          service = "huggingface",
+          history = NULL,
+          stream = FALSE,
+          model = "google/gemma-2-2b-it"
+        )
+      })
+    })
+  })
+
+  it ("works", {
+    expect_no_error({
+      chat(
+        prompt = "what is 1+1",
+        service = "huggingface",
+        history = NULL,
+        stream = FALSE,
+        model = "google/gemma-2-2b-it"
+      )
+    })
+  })
+})
