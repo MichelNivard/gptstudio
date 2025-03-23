7a9c1e483a8f16dae4e8ed98b126901e702e6a4e
Samuel Calderon <samuel.calderon@uarm.pe>
2025-03-23 11:01:58 -0500

    expect character type instead of arbitrary list of models

--- tests/testthat/test-models.R
+++ tests/testthat/test-models.R
@@ -12,68 +12,71 @@ mock_ollama_list <- function() {
 
 # Create test cases
 test_that("get_available_models works for openai", {
+  skip_on_cran()
   skip_on_ci()
   service <- "openai"
   models <- get_available_models(service)
-  expect_snapshot(models)
+  expect_type(models, "character")
 })
 
 test_that("get_available_models works for huggingface", {
+  skip_on_cran()
   skip_on_ci()
   service <- "huggingface"
   models <- get_available_models(service)
-  expect_equal(models, c("gpt2", "tiiuae/falcon-7b-instruct", "bigcode/starcoderplus"))
+  expect_type(models, "character")
 })
 
 test_that("get_available_models works for anthropic", {
+  skip_on_cran()
   skip_on_ci()
   service <- "anthropic"
   models <- get_available_models(service)
-  expect_equal(models, c(
-    "claude-3-5-sonnet-20240620", "claude-3-opus-20240229", "claude-3-sonnet-20240229",
-    "claude-3-haiku-20240307", "claude-2.1", "claude-instant-1.2"
-  ))
+  expect_type(models, "character")
 })
 
 test_that("get_available_models works for azure_openai", {
+  skip_on_cran()
+  skip_on_ci()
   service <- "azure_openai"
   models <- get_available_models(service)
   expect_equal(models, "Using ENV variables")
 })
 
 test_that("get_available_models works for perplexity", {
+  skip_on_cran()
   skip_on_ci()
   service <- "perplexity"
   models <- get_available_models(service)
-  expect_equal(models, c(
-    "llama-3-sonar-small-32k-chat", "llama-3-sonar-small-32k-online",
-    "llama-3-sonar-large-32k-chat", "llama-3-sonar-large-32k-online",
-    "llama-3-8b-instruct", "llama-3-70b-instruct", "mixtral-8x7b-instruct"
-  ))
+  expect_type(models, "character")
 })
 
 test_that("get_available_models works for ollama", {
+  skip_on_cran()
+  skip_on_ci()
   with_mocked_bindings(
     `ollama_is_available` = mock_ollama_is_available,
     `ollama_list` = mock_ollama_list,
     {
       service <- "ollama"
       models <- get_available_models(service)
-      expect_equal(models, c("ollama-3.5", "ollama-3", "ollama-2"))
+      expect_type(models, "character")
     }
   )
 })
 
 test_that("get_available_models works for cohere", {
+  skip_on_cran()
   skip_on_ci()
   service <- "cohere"
   models <- get_available_models(service)
-  expect_snapshot(models)
+  expect_type(models, "character")
 })
 
 test_that("get_available_models works for google", {
+  skip_on_cran()
   skip_on_ci()
   service <- "google"
   models <- get_available_models(service)
-  expect_snapshot(models)
+  expect_type(models, "character")
 })
